7ac9ad9bbc7542e0dee77ea9401b917e6e35f3c2
sunny aggarwal <sunnya@thoughtworks.com>
2012-07-09 08:45:44 +0530

    refactoring home page

--- header.php
+++ header.php
@@ -3,22 +3,32 @@
 <script src = "./javascripts/jquery.ui.widget.js"></script>
 <script src = "./javascripts/jquery.ui.tabs.js"></script>
 <link rel="stylesheet" href="./stylesheets/jquery.ui.all.css">
+<style>
+.menu{
+	width: 710px;
+}
+.menu_items{
+	background: #FFA500;
+	border: 0px;
+}
+</style>
 <script>
 	$(function() {
 		$( "#tabs" ).tabs();
 	});
 </script>
-	<img src="./images/logo.png" width="200px" hieght="200px"/>
-<div class="demo">
+	
+	
+<div class="menu">
 
 <div id="tabs">
-	<ul>
-		<li><a href="#tabs-1">Nunc tincidunt</a></li>
-		<li><a href="#tabs-2">Proin dolor</a></li>
+	<ul class="menu_items">
+		<li><a href="#tabs-1">Home</a></li>
+		<li><a href="#tabs-2">About Us</a></li>
 		<li><a href="#tabs-3">Aenean lacinia</a></li>
 	</ul>
 	<div id="tabs-1">
-		<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+		<?php include 'first_tab.html';?>
 	</div>
 	<div id="tabs-2">
 		<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
--- index.php
+++ index.php
@@ -1,11 +1,34 @@
+<style>
+html{
+	display: block;
+	max-width:1000px;
+	max-height:1600px;
+	margin: 0 0 0 200;
+}
+.banner{
+	border: 1px solid #AAAAAA;
+}
+body{
+	border: 1px solid #AAAAAA;
+}
+.main-area{
+	margin-left: auto;
+	width: 710;
+}
+
+</style>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Jobsera</title>
-<?php include "header.php"; ?>
 </head>
 <body>
-
+	<div class="parent-container">
+		<div class="banner"><img src="./images/logo.png" width="200px" hieght="200px"/></div>
+		<div class="side-bar"></div>
+		<div class="main-area"><?php include "header.php"; ?></div>
+	</div>
 </body>
 </html>
